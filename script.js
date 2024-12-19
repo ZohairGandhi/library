5d4e4c6c7400b6485b04bc920d9e8de1d75b54f0
Zohair Gandhi <87702549+ZohairGandhi@users.noreply.github.com>
2024-12-19 12:48:44 +1100

    Refactor Book & Library into classes

--- script.js
+++ script.js
@@ -1,18 +1,76 @@
-const myLibrary = [
-  { title: "1984", author: "George Orwell", pages: "328", haveRead: true },
-  { title: "Dune", author: "Frank Herbert", pages: "412", haveRead: false },
-];
-
-function Book(title, author, pages, haveRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.haveRead = haveRead;
+class Book {
+  constructor(title, author, pages, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+  }
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book);
+class Library {
+  constructor() {
+    this.myLibrary = [
+      new Book("1984", "George Orwell", 328, true),
+      new Book("Dune", "Frank Herbert", 412, false),
+    ];
+  }
+
+  addBookToLibrary(book) {
+    this.myLibrary.push(book);
+  }
+
+  displayLibrary() {
+    cardsSec.appendChild(newBtn);
+    let index = 0;
+
+    for (let book of this.myLibrary) {
+      const titleP = document.createElement("p");
+      const authorP = document.createElement("p");
+      const pagesP = document.createElement("p");
+      const chBoxLabel = document.createElement("label");
+      const chBox = document.createElement("input");
+      const removeBtn = document.createElement("button");
+
+      chBoxLabel.setAttribute("for", "haveRead");
+      chBox.setAttribute("type", "checkbox");
+      chBox.setAttribute("name", "haveRead");
+      chBox.setAttribute("id", "haveRead");
+      chBox.setAttribute("data-index", index);
+      removeBtn.setAttribute("data-index", index);
+
+      titleP.textContent = book.title;
+      authorP.textContent = book.author;
+      pagesP.textContent = book.pages;
+      chBoxLabel.textContent = "Read";
+      chBox.checked = book.haveRead;
+      removeBtn.textContent = "Remove";
+
+      chBox.addEventListener("change", (event) => {
+        const changeIdx = event.target.dataset.index;
+        this.myLibrary.at(changeIdx).haveRead = event.target.checked;
+      });
+
+      removeBtn.addEventListener("click", (event) => {
+        const removeIdx = event.target.dataset.index;
+        this.myLibrary.splice(removeIdx, 1);
+        cardsSec.innerHTML = "";
+        lib.displayLibrary();
+      });
+
+      chBoxLabel.appendChild(chBox);
+
+      const cardDiv = document.createElement("div");
+      cardDiv.classList.add("card");
+      cardDiv.append(titleP, authorP, pagesP, chBoxLabel, removeBtn);
+
+      cardsSec.appendChild(cardDiv);
+
+      index++;
+    }
+  }
 }
+
+const lib = new Library();
 
 const cardsSec = document.querySelector(".cards-sec");
 
@@ -22,57 +80,7 @@ const newBtnP = document.createElement("p");
 newBtnP.textContent = "New Book";
 newBtn.appendChild(newBtnP);
 
-function displayLibrary() {
-  cardsSec.appendChild(newBtn);
-  let index = 0;
-
-  for (let book of myLibrary) {
-    const titleP = document.createElement("p");
-    const authorP = document.createElement("p");
-    const pagesP = document.createElement("p");
-    const chBoxLabel = document.createElement("label");
-    const chBox = document.createElement("input");
-    const removeBtn = document.createElement("button");
-
-    chBoxLabel.setAttribute("for", "haveRead");
-    chBox.setAttribute("type", "checkbox");
-    chBox.setAttribute("name", "haveRead");
-    chBox.setAttribute("id", "haveRead");
-    chBox.setAttribute("data-index", index);
-    removeBtn.setAttribute("data-index", index);
-
-    titleP.textContent = book.title;
-    authorP.textContent = book.author;
-    pagesP.textContent = book.pages;
-    chBoxLabel.textContent = "Read";
-    chBox.checked = book.haveRead;
-    removeBtn.textContent = "Remove";
-
-    chBox.addEventListener("change", (event) => {
-      const changeIdx = event.target.dataset.index;
-      myLibrary.at(changeIdx).haveRead = event.target.checked;
-    });
-
-    removeBtn.addEventListener("click", (event) => {
-      const removeIdx = event.target.dataset.index;
-      myLibrary.splice(removeIdx, 1);
-      cardsSec.innerHTML = "";
-      displayLibrary();
-    });
-
-    chBoxLabel.appendChild(chBox);
-
-    const cardDiv = document.createElement("div");
-    cardDiv.classList.add("card");
-    cardDiv.append(titleP, authorP, pagesP, chBoxLabel, removeBtn);
-
-    cardsSec.appendChild(cardDiv);
-
-    index++;
-  }
-}
-
-displayLibrary();
+lib.displayLibrary();
 
 const modal = document.querySelector("dialog");
 const form = document.querySelector("form");
@@ -90,9 +98,9 @@ addBtn.addEventListener("click", () => {
   const pages = document.querySelector("#form-pages").value;
   const haveRead = document.querySelector("#form-haveRead");
 
-  addBookToLibrary(new Book(title, author, pages, haveRead.checked));
+  lib.addBookToLibrary(new Book(title, author, pages, haveRead.checked));
 
   cardsSec.innerHTML = "";
 
-  displayLibrary();
+  lib.displayLibrary();
 });
